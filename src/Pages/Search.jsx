74974bffdcb43533a22b114e0c4803f059559c5c
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-22 15:02:31 -0600

    Added search by hitting enter

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -3,13 +3,21 @@ import MovieComp from "../components/MovieComp";
 import MovieSkeleton from "../components/MovieSkeleton";
 import { useEffect } from "react";
 
+
 function Search({ click, movieArray, loadingState }) { // Destructure props
     console.log("Made it to Search")
     console.log(movieArray)
     const [query, setQuery] = useState("")
     const searchTerms = () => {
-        setQuery(document.getElementById("search__input").value)
-        
+        setQuery(document.getElementById("search__input").value);
+    }
+    const enterSearch = (event) => {
+        if (event.key === "Enter") {
+            searchTerms();
+        } else {
+            return
+        }
+
     }
 
     useEffect(() => {
@@ -21,7 +29,7 @@ function Search({ click, movieArray, loadingState }) { // Destructure props
         <>
             <header className="search__bar">
                 <div className="search">
-                    <input type="text" name="landing__search" id="search__input" placeholder="Search movies now" className="landing__search--input" />
+                    <input type="text" name="landing__search" onKeyDown={enterSearch} id="search__input" placeholder="Search movies now" className="landing__search--input" />
                     <button className="landing__search--btn" onClick={searchTerms}>
                         <svg className="mag-glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" />
