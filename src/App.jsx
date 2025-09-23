b3db312d3268c4738d724d8515663ed0ab2efe1e
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-23 13:22:28 -0600

    implemented a filter function that filters the array. now i have to figure out how to get it to reload the movies every time

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,64 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
-
-import Nav from './components/Nav';
-import './App.css';
-import Landing from './Pages/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import axios from 'axios';
+import './App.css';
+import Nav from './components/Nav';
+import Landing from './Pages/Landing';
 import Search from './Pages/Search';
 import Movie from './Pages/Movie';
-import axios from 'axios';
 
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 let currentDate = new Date().toJSON().slice(0, 10);
 
-// const API_OPTIONS = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: `Bearer ${API_KEY}`
-//   }
-// }
-
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [movieList, setMovieList] = useState([]);
-  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false); //sets loading state which is attached to the skeleton movie cards in search.jsx
+  const [movieList, setMovieList] = useState([]); //sets array from API
+  const [query, setQuery] = useState("") //sets value of query and gets updated by a function in the search.jsx called searchTerms
 
 
-  console.log(currentDate);
   const fetchMovies = async (query = '') => {
-    setIsLoading(true);
-
+    setIsLoading(true); //turns 'on' the loading state, while this is true the search component will display skeleton movie cards
 
     try {
-      const endpoint = query ?
-        `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}`
-        : `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&primary_release_date.lte=${currentDate}&language=en-US&sort_by=primary_release_date.desc`;
+      const endpoint = query ? //does query exist?
+        `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}` // if yes set endpoint value to this
+        : `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&primary_release_date.lte=${currentDate}&language=en&sort_by=primary_release_date.desc`; //if no set endpoint value to this 
 
-      const response = await axios.get(endpoint);
-      setMovieList(response.data.results);
+      const response = await axios.get(endpoint); //fetch api based on whether there is not a search query. if no query, it renders all movies released most recently
+      setMovieList(response.data.results); // sets movieList to the results from the API
     }
+
     catch (error) {
-      console.error(`Error fetching movies: ${error}`);
-    } finally {
-      setIsLoading(false);
+      console.error(`Error fetching movies: ${error}`); //if the try section fails, console log the error you encountered that caused it to fail
     }
-    
+
+    finally {
+      setIsLoading(false);//turns 'off' the loading state. search component will no longer display skeleton cards
+    }
+
   };
 
 
 
-  useEffect(() => {
+  useEffect(() => {//renders the fetchMovies funtion on page load and when the value of query changes.
     fetchMovies(query);
   }, [query])
 
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/search' element={<Search  click={fetchMovies} movieArray={movieList} loadingState={isLoading} />} />
+    <BrowserRouter> {/* Enables routing*/}
+      <Nav /> {/* renders the nav component */}
+      <Routes>  {/* says that this section will be subject to routing */}
+        <Route path='/' element={<Landing />} /> 
+        {/* Sets the route '/' and says that what displays in it will be the landing.jsx component */}
+
+        <Route path='/search' element={<Search click={fetchMovies} movieArray={movieList} loadingState={isLoading} />} /> 
+        {/* ^^ sets the route '/search' and says that what it displays is the search.jsx component. It also passes in three properties to the search component 
+        Property 1: click = the function fetchMovies. This is so that the search function can exist in the search component.
+        Property 2: movieArray=movieList. This passes the movie API results array on to the search component.
+        Property 3: loadingState=isLoading. this passes the value of the isLoading useState to the component so that it knows when to render the skeleton cards*/}
+
+
         {/* <Route path='/movie' element={<Movie />} /> */}
       </Routes>
 
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,58 +1,81 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieComp from "../components/MovieComp";
 import MovieSkeleton from "../components/MovieSkeleton";
-import { useEffect } from "react";
 
 
-function Search({ click, movieArray, loadingState, filterFunc }) { // Destructure props
-    console.log("Made it to Search")
-    console.log(movieArray)
-    const [query, setQuery] = useState("")
-    const [filteredMovies, setFilteredMovies] = useState(movieArray);
-    const [filter, setFilter] = useState("");
+function Search({ click, movieArray, loadingState }) {
+    console.log(movieArray);
+
+
+    const [query, setQuery] = useState(""); //copied over from App.jsx. Sets the query for the search function
+
     const searchTerms = () => {
-        setQuery(document.getElementById("search__input").value);
-    }
-    const enterSearch = (event) => {
-        if (event.key === "Enter") {
-            searchTerms();
-        } else {
-            return
-        }
-    }
-function setFilterMovies(event) {
-    setFilter(event.target.value)
-  }
-    const filterMovies = (movieArray) => {
-        let sortedMovies = movieArray; // Create a copy to avoid mutating the original array
-        if (filter === "RELEASE_OLD_TO_NEW") {
-            sortedMovies.sort((a, b) => Date.parse(a.release_date) - Date.parse(b.release_date));
-        } else if (filter === "RELEASE_NEW_TO_OLD") {
-            sortedMovies.sort((a, b) => Date.parse(b.release_date) - Date.parse(a.release_date));
-        } else if (filter === "RATING_HIGH_TO_LOW") {
-            sortedMovies.sort((a, b) => b.popularity - a.popularity);
-        } else if (filter === "RATING_LOW_TO_HIGH") {
-            sortedMovies.sort((a, b) => a.popularity - b.popularity);
-        }
-        return sortedMovies;
+        setQuery(document.getElementById("search__input").value); //sets the value of query to what's in the text box. the value is encoded to URI by the search function
     };
 
-    useEffect(() => {
+    const enterSearch = (event) => { //runs every time a key is pressed down in the input text box
+        if (event.key === "Enter") { //checks if the key pressed was the "enter" key
+            searchTerms(); // if it is enter, it runs the searchTerms function and sets the value of query to the text in the textbox
+        }
+    };//otherwise the function does nothing
+    const filteredMovies = movieArray; // creates a copy of the movie api array
+
+    const filterResults = (event) => {
+        const filter = event.target.value || ""// sets value of filter to the value of the option selected, or if nothing selected, then blank string
+        if (filter === "" || filter === 'DEFAULT'){//if filter is not engaged (aka equals default value or is empty)
+            return filteredMovies//then do nothing and exit this function
+        } else if (filter === "RELEASE_OLD_TO_NEW"){ // if the filter is __ do this
+            console.log("release old to new")
+                filteredMovies.sort(
+                (a, b) => {
+                    return parseInt(a.release_date) - parseInt(b.release_date) //converts release date to integer and year, then sorts
+                })
+        } else if (filter === "RELEASE_NEW_TO_OLD"){// if the filter is __ do this
+            console.log("release new to old")
+            filteredMovies.sort(
+                (a, b) => {
+                    return parseInt(b.release_date) - parseInt(a.release_date)//converts release date to integer and year, then sorts
+                })
+        }else if (filter === "RATING_HIGH_TO_LOW"){// if the filter is __ do this
+            console.log("rating high to low")
+            filteredMovies.sort(
+                (a, b) => {
+                    return parseFloat(b.popularity) - parseFloat(a.popularity) // converts popularity to decimal, then sorts
+                })
+        }else if (filter === "RATING_LOW_TO_HIGH"){// if the filter is __ do this
+            console.log("rating low to high")
+            filteredMovies.sort(
+                (a, b) => {
+                    return parseFloat(a.popularity) - parseFloat(b.popularity)// converts popularity to decimal, then sorts
+                })
+        }
+        console.log(filteredMovies);
+        return filteredMovies;
+    };
+
+    useEffect(() => { // runs the search function (imported as a prop from App.jsx) every time the value of query changes
         click(query)
     },
-        [query])
+        [query]);
 
-    useEffect(() => {
-        const updatedMovies = filterMovies(movieArray);
-        setFilteredMovies(updatedMovies);
-    }, [filter, movieArray]); // Depend on both filter and movieArray
 
     return (
         <>
             <header className="search__bar">
                 <div className="search">
-                    <input type="text" name="landing__search" onKeyDown={enterSearch} id="search__input" placeholder="Search movies now" className="landing__search--input" />
-                    <button className="landing__search--btn" onClick={searchTerms}>
+                    <input type="text"
+                        name="landing__search"
+                        onKeyDown={enterSearch}
+                        /*enables the check for enter funtion, triggers the click useEffect */
+                        id="search__input"
+                        placeholder="Search movies now"
+                        className="landing__search--input"
+                    />
+                    <button
+                        className="landing__search--btn"
+                        onClick={searchTerms}
+                    /*enables the value of query to be updated. triggers the click useEffect */
+                    >
                         <svg className="mag-glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 
                             533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 
@@ -66,20 +89,34 @@ function setFilterMovies(event) {
                     <div className="row movies__row">
                         <div className="filter__search--wrapper">
                             <p>Filter Movies By:</p>
-                            <select onChange={setFilterMovies} name="filter__search" id="filter__search" defaultValue="DEFAULT">
-                                <option value="DEFAULT">(no filter)</option>
-                                <option value="RELEASE_OLD_TO_NEW">Release Date: Old to New</option>
-                                <option value="RELEASE_NEW_TO_OLD">Release Date: New to Old</option>
-                                <option value="RATING_HIGH_TO_LOW">Rating: High to Low</option>
-                                <option value="RATING_LOW_TO_HIGH">Rating: Low to High</option>
+                            <select
+                                onChange={filterResults}
+                                name="filter__search"
+                                id="filter__search"
+                                defaultValue="DEFAULT">
+                                <option value="DEFAULT">
+                                    (no filter)
+                                </option>
+                                <option value="RELEASE_OLD_TO_NEW">
+                                    Release Year: Old to New
+                                </option>
+                                <option value="RELEASE_NEW_TO_OLD">
+                                    Release Year: New to Old
+                                </option>
+                                <option value="RATING_HIGH_TO_LOW">
+                                    Rating: High to Low
+                                </option>
+                                <option value="RATING_LOW_TO_HIGH">
+                                    Rating: Low to High
+                                </option>
                             </select>
                         </div>
                         <div className="movies">
-                            {loadingState ? (
-                                // Render skeletons if loading
+                            {loadingState ? ( //is loading state true?
+                                // Render skeletons if true
                                 new Array(8).fill(0).map((_, index) => <MovieSkeleton key={index} />)
                             ) : (
-                                // Render movies if not loading
+                                // Render movies if false
                                 <MovieComp movies={filteredMovies} />
                             )}
                         </div>
