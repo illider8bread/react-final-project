6c63c9b1d514de87ae876d61f91599a3aebd7875
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-23 09:45:22 -0600

    Changed search function on landing page to just redirect to search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 import Nav from './components/Nav';
 import './App.css';
 import Landing from './Pages/Landing';
@@ -22,38 +24,41 @@ let currentDate = new Date().toJSON().slice(0, 10);
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [movieList, setMovieList] = useState([]);
- console.log(currentDate);
+  const [query, setQuery] = useState("")
+  
+  console.log(currentDate);
   const fetchMovies = async (query = '') => {
     setIsLoading(true);
-
-
-  try {
-    const endpoint = query ?
+    
+    
+    try {
+      const endpoint = query ?
       `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}`
       : `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&primary_release_date.lte=${currentDate}&language=en-US&sort_by=primary_release_date.desc`;
-
-    const response = await axios.get(endpoint);
-    setMovieList(response.data.results);
-  } 
+      
+      const response = await axios.get(endpoint);
+      setMovieList(response.data.results);
+    }
     catch (error) {
       console.error(`Error fetching movies: ${error}`);
-    }finally {
+    } finally {
       setIsLoading(false);
     }
   };
-
+  
+  
   useEffect(() => {
-    fetchMovies();
-  }, [])
+    fetchMovies(query);
+  }, [query])
   useEffect(() => {
     console.log(movieList);
-}, [movieList]);
-
+  }, [movieList]);
+  
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path='/' element={<Landing />} />
+        <Route path='/' element={<Landing/>} />
         <Route path='/search' element={<Search click={fetchMovies} movieArray={movieList} loadingState={isLoading} />} />
         {/* <Route path='/movie' element={<Movie />} /> */}
       </Routes>
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,12 +1,11 @@
+import { Link } from "react-router";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 
 const Landing = () => {
-    const navigate = useNavigate(); // Correctly using the hook inside the component
-    function LandingSearch() {
-        console.log("Landing Search");
-        navigate('/search');
-    }
+    
+
     return (
         <header className="landing__page">
             <div className="container">
@@ -21,9 +20,10 @@ const Landing = () => {
                             </h4>
                         </div>
                         <div className="landing__search--wrapper">
-                            <input type="text" name="landing__search" id="landing__search--input" className="landing__search--input" placeholder="Search movies now" />
-                            <button className="landing__search--btn" onClick={LandingSearch()}>
-                                <svg className="mag-glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" /></svg>
+                            <button className="landing__search--btn">
+                                <Link to="/search">
+                                    <svg className="mag-glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" /></svg>
+                                </Link>
                             </button>
                         </div>
                     </section>
